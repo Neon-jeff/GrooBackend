87dbc862cded92f1daeeb2251a2b648c7c5111d7
Neon-jeff <omaksteejay@gmail.com>
2023-12-29 19:00:05 +0100

    PDF Download

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
  echo "BUILD START"
+ sudo apt install libcairo2-dev pkg-config python3-dev
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
  python3.9 manage.py migrate
